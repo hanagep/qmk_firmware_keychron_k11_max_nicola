89a71867f181910b86e05e30426a5a90c07902af
hanage <hanangelion-nico@yahoo.co.jp>
2026-06-14 12:07:50 +0900

    fixed nicola keymap

--- keyboards/keychron/k11_max/ansi_encoder/white/keymaps/nicola/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/white/keymaps/nicola/keymap.c
@@ -24,9 +24,7 @@ enum layers{
     MAC_BASE,
     WIN_BASE,
     _NICOLA,
-    MAC_FN1,
-    WIN_FN1,
-    FN2,
+    FN1,
 };
 
 enum custom_keycodes {
@@ -35,8 +33,9 @@ enum custom_keycodes {
     KC_WIN_SHFT_LEFT,
     KC_TEAM_MUTE,
     TG_JP,
-    EISU_FN2,
-    KANA_FN2,
+    EISU_FN1,
+    KANA_FN1,
+    KC_MDN_PRV
 };
 
 bool us_emu_enabled = false;
@@ -56,46 +55,32 @@ tap_dance_action_t tap_dance_actions[] = {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_69_ansi(
-        KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_WIN_SHFT_LEFT,
-        KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_DEL,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_ESC,  KC_LOPTN, KC_LCMMD,          KC_SPC,           EISU_FN2, KANA_FN2,       KC_SPC,            KC_TEAM_MUTE,       KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,             KC_BSPC, KC_MUTE,
+        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_WIN_SHFT_LEFT,
+        KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,  KC_DEL,
+        KC_LSFT, KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,     KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,  KC_UP,
+        KC_ESC,  KC_LOPTN, KC_LCMMD,          KC_SPC,  EISU_FN1, KANA_FN1, KC_SPC,           KC_TEAM_MUTE,                         KC_LEFT,  KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
-        KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_WIN_SHFT_LEFT,
-        KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_DEL,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_ESC,  KC_LWIN,  KC_LALT,           KC_SPC,           EISU_FN2, KANA_FN2,       KC_SPC,            KC_TEAM_MUTE,       KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,             KC_BSPC, KC_MUTE,
+        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_WIN_SHFT_LEFT,
+        KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,  KC_DEL,
+        KC_LSFT, KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,     KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,  KC_UP,
+        KC_ESC,  KC_LWIN,  KC_LALT,           KC_SPC,  EISU_FN1, KANA_FN1, KC_SPC,           KC_TEAM_MUTE,                         KC_LEFT,  KC_DOWN, KC_RGHT),
 
     [_NICOLA] = LAYOUT_69_ansi(
-        KC_GRV,  NG_1,     NG_2,     NG_3,    NG_4,    NG_5,    NG_Q,     NG_W,    NG_E,    NG_R,    NG_T,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,  NG_Q,     NG_W,     NG_E,    NG_R,    NG_T,    NG_Y,     NG_U,    NG_I,    NG_O,    NG_P,     NG_LBRC,  NG_RBRC,  KC_BSLS,          KC_WIN_SHFT_LEFT,
-        KC_TRNS, NG_A,     NG_S,     NG_D,    NG_F,    NG_G,              NG_H,    NG_J,    NG_K,    NG_L,     NG_SCLN,  NG_QUOT,  KC_ENT,           KC_DEL,
-        KC_LSFT,           NG_Z,     NG_X,    NG_C,    NG_V,    NG_B,     NG_B,    NG_N,    NG_M,    NG_COMM,  NG_DOT,   NG_SLSH,  NG_SHFTR, KC_UP,
-        KC_ESC,  KC_TRNS,  KC_TRNS,           NG_SHFTL,         EISU_FN2, KANA_FN2,       NG_SHFTR,          KC_TEAM_MUTE,       KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_GRV,  NG_1,     NG_2,     NG_3,    NG_4,    NG_5,     NG_Q,     NG_W,    NG_E,    NG_R,    NG_T,    KC_MINS,  KC_EQL,             KC_BSPC, KC_MUTE,
+        KC_TAB,  NG_Q,     NG_W,     NG_E,    NG_R,    NG_T,               NG_Y,    NG_U,    NG_I,    NG_O,    NG_P,     NG_LBRC,  NG_RBRC,  KC_BSLS, KC_WIN_SHFT_LEFT,
+        KC_TRNS, NG_A,     NG_S,     NG_D,    NG_F,    NG_G,               NG_H,    NG_J,    NG_K,    NG_L,    NG_SCLN,  NG_QUOT,            KC_ENT,  KC_DEL,
+        KC_LSFT, NG_Z,     NG_X,     NG_C,    NG_V,    NG_B,     NG_B,     NG_N,    NG_M,    NG_COMM, NG_DOT,  NG_SLSH,            NG_SHFTR, KC_UP,
+        KC_ESC,  KC_TRNS,  KC_TRNS,           NG_SHFTL,EISU_FN1, KANA_FN1, NG_SHFTR,         KC_TEAM_MUTE,                         KC_LEFT,  KC_DOWN, KC_RGHT),
 
-    [MAC_FN1] = LAYOUT_69_ansi(
-        KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,      KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          BL_TOGG,
-        KC_KANA, TG_JP,    KC_HOME,  KC_UP,   KC_END,  _______, _______,  _______,    _______, _______,  _______,  KC_PSCR,  _______,  _______,          _______,
-        BL_TOGG, LALT(KC_GRV), KC_LEFT, KC_DOWN, KC_RGHT, _______,           _______, KC_INS,  KC_HOME, KC_PGUP,  KC_BSPC,  _______,  _______,          _______,
-        _______,           KC_VOLD,  BT_HST1, BT_HST2, BT_HST3, BAT_LVL,  BAT_LVL,    KC_DEL,  KC_END,  KC_PGDN,  _______,  _______,  _______, _______,
-        KC_CAPS, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______),
-
-    [WIN_FN1] = LAYOUT_69_ansi(
-        KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,      KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          BL_TOGG,
-        KC_KANA, TG_JP,    KC_HOME,  KC_UP,   KC_END,  _______, _______,  _______,    _______, _______,  _______,  KC_PSCR,  _______,  _______,          _______,
-        BL_TOGG, LALT(KC_GRV), KC_LEFT, KC_DOWN, KC_RGHT, _______,           _______, KC_INS,  KC_HOME, KC_PGUP,  KC_BSPC,  _______,  _______,          _______,
-        _______,           KC_VOLD,  BT_HST1, BT_HST2, BT_HST3, BAT_LVL,  BAT_LVL,    KC_DEL,  KC_END,  KC_PGDN,  _______,  _______,  _______, _______,
-        KC_CAPS, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______),
-
-    [FN2] = LAYOUT_69_ansi(
-        KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,      KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          BL_TOGG,
-        KC_KANA, _______,    KC_HOME,  KC_UP,   KC_END,  _______, _______,  _______,   _______, _______,  _______,  KC_PSCR,  _______,  _______,          _______,
-        KC_CAPS, LALT(KC_GRV), KC_LEFT, KC_DOWN, KC_RGHT, _______,           _______, KC_INS,  KC_HOME, KC_PGUP,  KC_BSPC,  _______,  _______,          _______,
-        _______,           KC_VOLD,  BT_HST1, BT_HST2, BT_HST3, BAT_LVL,  BAT_LVL,    KC_DEL,  KC_END,  KC_PGDN,  _______,  _______,  _______, BL_UP,
-        TG_JP, _______,  _______,           _______,          _______,  _______,          _______,           _______,            BL_TOGG, BL_DOWN, BL_STEP)
+    [FN1] = LAYOUT_69_ansi(
+        KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,            _______, BL_TOGG,
+        KC_KANA, _______,  KC_HOME,  KC_UP,   KC_END,  _______,            _______, _______, _______, _______,  _______,  KC_PSCR,  _______, _______, _______,
+        KC_CAPS, LALT(KC_GRV), KC_LEFT, KC_DOWN, KC_RGHT, _______,         _______, KC_INS,  KC_HOME, KC_PGUP,  KC_BSPC,  _______,  _______,          _______,
+        _______, KC_VOLD,  BT_HST1, BT_HST2, BT_HST3, BAT_LVL, KC_MDN_PRV, _______, KC_DEL,  KC_END,  KC_PGDN,  _______,            _______, BL_UP,
+        TG_JP, _______,  _______,             _______, _______,  _______,  _______,          _______,                               BL_TOGG, BL_DOWN, BL_STEP)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -103,9 +88,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [_NICOLA]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [MAC_FN1]  = { ENCODER_CCW_CW(BL_DOWN, BL_UP)},
-    [WIN_FN1]  = { ENCODER_CCW_CW(BL_DOWN, BL_UP)},
-    [FN2]      = { ENCODER_CCW_CW(BL_DOWN, BL_UP)},
+    [FN1]      = { ENCODER_CCW_CW(BL_DOWN, BL_UP)},
 };
 #endif // ENCODER_MAP_ENABLE
 // clang-format on
@@ -119,8 +102,8 @@ void keyboard_post_init_user(void) {
     nicola_off();
 }
 
-static uint16_t eisu_fn2_timer;
-static uint16_t kana_fn2_timer;
+static uint16_t eisu_fn1_timer;
+static uint16_t kana_fn1_timer;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_keychron_common(keycode, record)) {
@@ -128,13 +111,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     if (record->event.pressed) {
         switch (keycode) {
-            case EISU_FN2:
-                eisu_fn2_timer = timer_read();
-                layer_on(FN2);
+            case EISU_FN1:
+                eisu_fn1_timer = timer_read();
+                layer_on(FN1);
                 return false;
-            case KANA_FN2:
-                kana_fn2_timer = timer_read();
-                layer_on(FN2);
+            case KANA_FN1:
+                kana_fn1_timer = timer_read();
+                layer_on(FN1);
                 return false;
             case KC_WIN_SHFT_LEFT:
                 register_code(KC_LGUI);
@@ -151,6 +134,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(KC_K);
                 unregister_code(KC_LALT);
                 unregister_code(KC_LGUI);
+                return false;
+            case KC_MDN_PRV:
+                register_code(KC_LSFT);
+                register_code(KC_LCTL);
+                wait_ms(100);
+                tap_code(KC_V);
+                unregister_code(KC_LCTL);
+                unregister_code(KC_LSFT);
                 return false;
             case TG_JP:
                 us_emu_enabled = !us_emu_enabled;
@@ -174,26 +165,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
     } else {
         switch (keycode) {
-            case EISU_FN2:
-                layer_off(FN2);
-                if (timer_elapsed(eisu_fn2_timer) < TAPPING_TERM) {
+            case EISU_FN1:
+                layer_off(FN1);
+                if (timer_elapsed(eisu_fn1_timer) < TAPPING_TERM) {
                     layer_off(_NICOLA);
                     nicola_off();
                     tap_code(KC_LNG2);
                 }
                 return false;
-            case KANA_FN2:
-                layer_off(FN2);
-                if (timer_elapsed(kana_fn2_timer) < TAPPING_TERM) {
+            case KANA_FN1:
+                layer_off(FN1);
+                if (timer_elapsed(kana_fn1_timer) < TAPPING_TERM) {
+                    /*
                     if (nicola_state()) {
                         layer_off(_NICOLA);
                         nicola_off();
                         tap_code(KC_LNG2); // IME OFF
+                    
                     } else {
+                    */
                         layer_on(_NICOLA);
                         nicola_on();
                         tap_code(KC_LNG1); // IME ON
-                    }
+                    //}
                 }
                 return false;
         }
@@ -223,11 +217,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void fn_ime_finished (tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) {
         if (state->pressed) {
-            if (layer_state_is(WIN_BASE)) {
-                layer_on(WIN_FN1);
-            } else {
-                layer_on(MAC_FN1);
-            }
+            layer_on(FN1);
         } else {
             layer_off(_NICOLA);
             nicola_off();
@@ -241,6 +231,5 @@ void fn_ime_finished (tap_dance_state_t *state, void *user_data) {
 }
 
 void fn_ime_reset (tap_dance_state_t *state, void *user_data) {
-    layer_off(MAC_FN1);
-    layer_off(WIN_FN1);
+    layer_off(FN1);
 }
