97725f4efff72e7cb328cb13720da742e6b21970
hanage <hanangelion-nico@yahoo.co.jp>
2026-06-14 19:39:35 +0900

    updated keymaps

--- keyboards/keychron/k11_max/ansi_encoder/white/keymaps/nicola/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/white/keymaps/nicola/keymap.c
@@ -85,10 +85,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [_NICOLA]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [FN1]      = { ENCODER_CCW_CW(BL_DOWN, BL_UP)},
+    [MAC_BASE] = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)},
+    [WIN_BASE] = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)},
+    [_NICOLA]  = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)},
+    [FN1]      = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 #endif // ENCODER_MAP_ENABLE
 // clang-format on
