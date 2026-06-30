8a8d4bcc14781f319aeb360c9a551ae709546c41
hanagep <136151347+hanagep@users.noreply.github.com>
2026-06-30 19:30:32 +0900

    Update Nicola keymap to include additional keys

--- keyboards/keychron/k11_max/ansi_encoder/white/keymaps/nicola/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/white/keymaps/nicola/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_LWIN,  KC_LALT,           KC_SPC,  EISU_FN1, KANA_FN1, KC_SPC,           KC_TEAM_MUTE,                         KC_LEFT,  KC_DOWN, KC_RGHT),
 
     [_NICOLA] = LAYOUT_69_ansi(
-        KC_GRV,  NG_1,     NG_2,     NG_3,    NG_4,    NG_5,     NG_Q,     NG_W,    NG_E,    NG_R,    NG_T,    KC_MINS,  KC_EQL,             KC_BSPC, KC_MUTE,
+        KC_GRV,  NG_1,     NG_2,     NG_3,    NG_4,    NG_5,     NG_6,     NG_7,    NG_8,    NG_9,    NG_0,    KC_MINS,  KC_EQL,             KC_BSPC, KC_MUTE,
         KC_TAB,  NG_Q,     NG_W,     NG_E,    NG_R,    NG_T,               NG_Y,    NG_U,    NG_I,    NG_O,    NG_P,     NG_LBRC,  NG_RBRC,  KC_BSLS, KC_WIN_SHFT_LEFT,
         KC_TRNS, NG_A,     NG_S,     NG_D,    NG_F,    NG_G,               NG_H,    NG_J,    NG_K,    NG_L,    NG_SCLN,  NG_QUOT,            KC_ENT,  KC_DEL,
         KC_LSFT, NG_Z,     NG_X,     NG_C,    NG_V,    NG_B,     NG_B,     NG_N,    NG_M,    NG_COMM, NG_DOT,  NG_SLSH,            NG_SHFTR, KC_UP,
